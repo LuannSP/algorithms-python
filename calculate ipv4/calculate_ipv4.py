0c80a9c6a1ca2822ab692016a79209976bc31953
LuannSP <luann2v2v@hotmail.com>
2022-11-27 13:00:03 -0300

    type hints calculate ipv4 fix

--- calculate ipv4/calculate_ipv4.py
+++ calculate ipv4/calculate_ipv4.py
@@ -1,9 +1,9 @@
 import re
-
+from typing import Any
 
 class CalcIpv4:
 
-    def __init__(self, ip, mask=None, prefix=None) -> None:
+    def __init__(self, ip: str, mask: str = None, prefix: int = None) -> None:
         self.ip = ip
         self.mask = mask
         self.prefix = prefix
@@ -12,34 +12,34 @@ class CalcIpv4:
         self._set_network()
 
     @property
-    def network(self):
+    def network(self) -> str:
         return self._network
 
     @property
-    def broadcast(self):
+    def broadcast(self) -> str:
         return self._broadcast
 
     @property
-    def number_ip(self):
+    def number_ip(self) -> Any:
         return self._get_number_ip()
 
     @property
-    def ip(self):
+    def ip(self) -> Any:
         return self._ip
 
     @ip.setter
-    def ip(self, value):
+    def ip(self, value: str) -> None:
         if not self._valid_ip(value):
             raise ValueError('invalid IP')
         self._ip = value
         self._ip_bin = self._ip_to_bin(value)
 
     @property
-    def mask(self):
+    def mask(self) -> (Any | str):
         return self._mask
 
     @mask.setter
-    def mask(self, value):
+    def mask(self, value: str) -> None:
         if not value:
             return
         if not self._valid_ip(value):
@@ -50,11 +50,11 @@ class CalcIpv4:
             self.prefix = self._mask_bin.count('1')
 
     @property
-    def prefix(self):
+    def prefix(self) -> int:
         return self._prefix
 
     @prefix.setter
-    def prefix(self, value):
+    def prefix(self, value: int) -> None:
         if not value:
             return
         if not isinstance(value, int):
@@ -67,35 +67,35 @@ class CalcIpv4:
             self._mask = self._bin_to_ip(self._mask_bin)
 
     @staticmethod
-    def _valid_ip(ip):
+    def _valid_ip(ip: Any) -> Any:
         regexp = re.compile(
             r'^([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})$')
         if regexp.search(ip):
             return True
 
     @staticmethod
-    def _ip_to_bin(ip):
+    def _ip_to_bin(ip: Any) -> str:
         blocks = ip.split('.')
         blocks_binary = [bin(int(x))[2:].zfill(8) for x in blocks]
         return ''.join(blocks_binary)
 
     @staticmethod
-    def _bin_to_ip(ip):
+    def _bin_to_ip(ip: Any) -> str:
         n = 8
         blocks = [str(int(ip[i:n+i], 2)) for i in range(0, 32, n)]
         return '.'.join(blocks)
 
-    def _set_broadcast(self):
+    def _set_broadcast(self) -> str:
         host_bits = 32 - self.prefix
         self._broadcast_bin = self._ip_bin[:self.prefix] + (host_bits * '1')
         self._broadcast = self._bin_to_ip(self._broadcast_bin)
         return self._broadcast
 
-    def _set_network(self):
+    def _set_network(self) -> str:
         host_bits = 32 - self.prefix
         self._network_bin = self._ip_bin[:self.prefix] + (host_bits * '0')
         self._network = self._bin_to_ip(self._network_bin)
         return self._network
 
-    def _get_number_ip(self):
+    def _get_number_ip(self) -> Any:
         return 2 ** (32 - self.prefix)
